1937fe70179cb9ea069faac0a23f3674119a6452
Henrique <167441138+henrik12343@users.noreply.github.com>
2026-04-07 09:00:11 -0300

    Add files via upload

---
+++ lampada.js
@@ -0,0 +1,25 @@
+const turnOff = document.getElementById("turnOff");
+const turnON = document.getElementById("turnOn");
+const lamp = document.getElementById("lamp");
+
+function lampOn(){
+    if(!islampBroken()){
+        lamp.src = "./IMG/ligada.jpg";
+    }
+}
+function lampOff(){
+    if(!islampBroken()){
+        lamp.src = "./IMG/desligada.jpg";
+    }
+}
+function lampBroken(){
+    lamp.src = "./IMG/quebrada.jpg";
+}
+function islampBroken(){
+    return lamp.src.includes("quebrada");
+}
+turnON.addEventListener("click",lampOn);
+turnOff.addEventListener("click",lampOff);
+lamp.addEventListener("mouseover", lampOn);
+lamp.addEventListener("mouseleave", lampOff);
+lamp.addEventListener("dblclick", lampBroken);
